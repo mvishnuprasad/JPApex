b610736d9543187f412c554a9914ae305d21435b
vishnuprasad <vpm735@gmail.com>
2025-06-26 10:02:16 +0530

    init

---
+++ JPApex/JPApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexApp.swift
+//  JPApex
+//
+//  Created by vishnuprasad on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
